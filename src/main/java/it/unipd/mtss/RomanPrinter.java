0317a670e41129503f74789e447479060ffc8f3c
marco-beltrame <mcanussio@gmail.com>
2026-05-02 18:09:39 +0200

    Primi 6 numeri e test

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -15,13 +15,22 @@ public class RomanPrinter {
         " |_____|"
     };
 
+    private static final String[] ASCII_V = {
+        "__      __",
+        "\\ \\    / /",
+        " \\ \\  / / ",
+        "  \\ \\/ /  ",
+        "   \\  /   ",
+        "    \\/    "
+    };
+
     public static String print(int num) {
         return printAsciiArt(IntegerToRoman.convert(num));
     }
 
     private static String printAsciiArt(String romanNumber) {
         StringBuilder result = new StringBuilder();
-        for (int row = 0; row < 6; row++) {  // 6 righe!
+        for (int row = 0; row < 6; row++) {
             for (char c : romanNumber.toCharArray()) {
                 result.append(getAsciiChar(c)[row]);
                 result.append("  ");
@@ -34,6 +43,7 @@ public class RomanPrinter {
     private static String[] getAsciiChar(char c) {
         switch (c) {
             case 'I': return ASCII_I;
+            case 'V': return ASCII_V;
             default: throw new IllegalArgumentException(
                 "Carattere romano non riconosciuto: " + c
             );
