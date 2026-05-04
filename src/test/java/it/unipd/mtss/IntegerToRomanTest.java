aa4975d43b03d9d700653adf8ed4e94e63259de3
MasoStefano4 <masostefano4@gmail.com>
2026-05-04 20:14:10 +0200

    Numeri fino al 50

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -64,6 +64,27 @@ public class IntegerToRomanTest {
     }
 
     @Test
+    public void convertFourtyfour_shouldReturnXLIV() {
+        int input = 44;
+        String result = IntegerToRoman.convert(input);
+        assertEquals("XLIV", result);
+    }
+
+    @Test
+    public void convertFourtynine_shouldReturnXLIX() {
+        int input = 49;
+        String result = IntegerToRoman.convert(input);
+        assertEquals("XLIX", result);
+    }
+
+    @Test
+    public void convertFifty_shouldReturnL() {
+        int input = 50;
+        String result = IntegerToRoman.convert(input);
+        assertEquals("L", result);
+    }
+
+    @Test
     public void convertZero_shouldThrowException() {
         int input = 0;
         Executable result = () -> IntegerToRoman.convert(input);
@@ -79,7 +100,7 @@ public class IntegerToRomanTest {
 
     @Test
     public void convertOverLimit_shouldThrowException() {
-        int input = 21;
+        int input = 51;
         Executable result = () -> IntegerToRoman.convert(input);
         assertThrows(IllegalArgumentException.class, result);
     }
